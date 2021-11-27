42a459e76ca86e97f5481d0fdc68526761a5b53f
LukasGasp <72341833+LukasGasp@users.noreply.github.com>
2021-11-27 22:56:13 +0100

    Video voll Funktionsfähig
    Klassen in der selben Datei. Nicht besonders Premium aber funktionfähig

--- lib/detail_page.dart
+++ lib/detail_page.dart
@@ -3,7 +3,6 @@ import 'navigation.dart';
 import 'data.dart';
 import 'constants.dart';
 import 'package:video_player/video_player.dart';
-import 'package:path/path.dart';
 
 class DetailPage extends StatelessWidget {
   final StationInfo stationInfo;
@@ -99,16 +98,6 @@ class DetailPage extends StatelessWidget {
                         },
                       ),
                       ElevatedButton(
-                        child: const Text('Play'),
-                        onPressed: () {
-                          if (_controller.value.isPlaying) {
-                            _controller.pause();
-                          } else {
-                            _controller.play();
-                          }
-                        },
-                      ),
-                      ElevatedButton(
                         child: const Text('Weiter'),
                         onPressed: () {
                           if (stationInfo.next != 0) {
@@ -132,41 +121,11 @@ class DetailPage extends StatelessWidget {
                   const Divider(color: Colors.black38),
                   const SizedBox(height: 32),
 
-                  Container(
-                    padding: const EdgeInsets.all(20),
-                    child: AspectRatio(
-                      aspectRatio: _controller.value.aspectRatio,
-                      child: Stack(
-                        alignment: Alignment.bottomCenter,
-                        children: <Widget>[
-                          AspectRatio(
-                            aspectRatio: _controller.value.aspectRatio,
-                            child: VideoPlayer(_controller),
-                          ),
-                          ClosedCaption(text: _controller.value.caption.text),
-                          _ControlsOverlay(controller: _controller),
-                          VideoProgressIndicator(_controller,
-                              allowScrubbing: true),
-                        ],
-                      ),
-                    ),
-                  ),
+                  // Video: Klassen unten
 
-                  // Video
-                  FutureBuilder(
-                      future: _initializeVideoPlayerfuture,
-                      builder: (context, snapshot) {
-                        if (_controller.value.isInitialized) {
-                          return AspectRatio(
-                            aspectRatio: _controller.value.aspectRatio,
-                            child: VideoPlayer(_controller),
-                          );
-                        } else {
-                          return const Center(
-                            child: CircularProgressIndicator(),
-                          );
-                        }
-                      }),
+                  _StationAssetVideo(
+                    videopath: stationInfo.video,
+                  ),
 
                   const SizedBox(height: 32),
                   const Divider(color: Colors.black38),
@@ -238,6 +197,70 @@ class DetailPage extends StatelessWidget {
   }
 }
 
+// Video stuff
+class _StationAssetVideo extends StatefulWidget {
+  final String videopath;
+
+  _StationAssetVideo({Key? key, required this.videopath}) : super(key: key);
+
+  @override
+  _StationAssetVideoState createState() =>
+      _StationAssetVideoState(videopath: videopath);
+}
+
+class _StationAssetVideoState extends State<_StationAssetVideo> {
+  late VideoPlayerController _controller;
+  final String videopath;
+
+  _StationAssetVideoState({Key? key, required this.videopath});
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.asset(videopath);
+
+    _controller.addListener(() {
+      setState(() {});
+    });
+    _controller.setLooping(false);
+    _controller.initialize().then((_) => setState(() {}));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.only(top: 20.0),
+          ),
+          Container(
+            padding: const EdgeInsets.all(0),
+            child: AspectRatio(
+              aspectRatio: _controller.value.aspectRatio,
+              child: Stack(
+                alignment: Alignment.bottomCenter,
+                children: <Widget>[
+                  VideoPlayer(_controller),
+                  _ControlsOverlay(controller: _controller),
+                  VideoProgressIndicator(_controller, allowScrubbing: true),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// Video Overlay
 class _ControlsOverlay extends StatelessWidget {
   const _ControlsOverlay({Key? key, required this.controller})
       : super(key: key);
@@ -245,12 +268,12 @@ class _ControlsOverlay extends StatelessWidget {
   static const _examplePlaybackRates = [
     0.25,
     0.5,
+    0.75,
     1.0,
+    1.25,
     1.5,
-    2.0,
-    3.0,
-    5.0,
-    10.0,
+    1.75,
+    2.0
   ];
 
   final VideoPlayerController controller;
@@ -285,7 +308,7 @@ class _ControlsOverlay extends StatelessWidget {
           alignment: Alignment.topRight,
           child: PopupMenuButton<double>(
             initialValue: controller.value.playbackSpeed,
-            tooltip: 'Playback speed',
+            tooltip: 'Wiedergabetempo',
             onSelected: (speed) {
               controller.setPlaybackSpeed(speed);
             },
