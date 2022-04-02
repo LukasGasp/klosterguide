bbb0746001f041412a985f8517a34b478a183cb1
Denormos <95382828+Denormos@users.noreply.github.com>
2022-04-02 21:39:13 +0200

    Audio removal und Video eingefügt
    Titel

--- lib/map.dart
+++ lib/map.dart
@@ -3,7 +3,6 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
 import 'package:klosterguide/constants.dart';
 import 'package:latlong2/latlong.dart';
-import 'package:audioplayers/audioplayers.dart';
 
 import 'data.dart';
 import 'detail_page.dart';
@@ -15,8 +14,6 @@ class Karte extends StatelessWidget {
 
   get localPath => null;
 
-  AudioPlayer audioPlayer = AudioPlayer();
-  AudioCache player = AudioCache(prefix: 'assets/audio/');
 
   @override
   Widget build(BuildContext context) {
@@ -57,73 +54,32 @@ class Karte extends StatelessWidget {
         body: Stack(
           alignment: Alignment.center,
           children: [
-            Positioned(
-              bottom: 10,
-              child: Container(
-                width: 200,
-                height: 100,
-                padding: EdgeInsets.only(left: 16, right: 16),
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.all(Radius.circular(50)),
-                  boxShadow: [BoxShadow(color: Colors.grey, blurRadius: 10.0, spreadRadius: 0.1)]
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    IconButton(
-                      onPressed: (){
-                        player.play('test.mp3');
-                      },
-
-                        icon: Icon(Icons.play_arrow, color: Colors.grey, size: 30 )),
-                    IconButton(
-                        onPressed: (){
-                          player.play('test.mp3');
-                        },
-
-                        icon: Icon(Icons.play_arrow, color: Colors.grey, size: 30 ))
-                  ],
-                )
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.only(bottom: 100),
-              child:FlutterMap(
-
-                options: MapOptions(
-                    center: LatLng(51.07761902538088, 6.752730047496091),
-                    zoom: 15.7,
-                    minZoom: 14,
-                    maxZoom: 18,
-                    swPanBoundary: LatLng(
-                      51.0727696061025,
-                      6.74033047115352,
-                    ),
-                    nePanBoundary: LatLng(51.0834444361947, 6.76094581023449),
-                    plugins: [const LocationMarkerPlugin()]),
-                layers: [
-                  TileLayerOptions(
-                    tileProvider: const AssetTileProvider(),
-                    maxZoom: 18.0,
-                    urlTemplate: 'assets/map/{z}/{x}/{y}.png',
-                    // For example purposes. It is recommended to use
-                    // TileProvider with a caching and retry strategy, like
-                    // NetworkTileProvider or CachedNetworkTileProvider
+            FlutterMap(
+              options: MapOptions(
+                  center: LatLng(51.07761902538088, 6.752730047496091),
+                  zoom: 15.7,
+                  minZoom: 14,
+                  maxZoom: 18,
+                  swPanBoundary: LatLng(
+                    51.0727696061025,
+                    6.74033047115352,
                   ),
-                  LocationMarkerLayerOptions(),
-                  MarkerLayerOptions(markers: markierungen),
-                ],
-              ),
+                  nePanBoundary: LatLng(51.0834444361947, 6.76094581023449),
+                  plugins: [const LocationMarkerPlugin()]),
+              layers: [
+                TileLayerOptions(
+                  tileProvider: const AssetTileProvider(),
+                  maxZoom: 18.0,
+                  urlTemplate: 'assets/map/{z}/{x}/{y}.png',
+                  // For example purposes. It is recommended to use
+                  // TileProvider with a caching and retry strategy, like
+                  // NetworkTileProvider or CachedNetworkTileProvider
+                ),
+                LocationMarkerLayerOptions(),
+                MarkerLayerOptions(markers: markierungen),
+              ],
             ),
           ],
         ));
   }
-  
-  void playAudio(){
-    playLocal() async {
-      int result = await audioPlayer.play(localPath, isLocal: true);
-    }
-  }
 }
-
--- lib/navigation.dart
+++ lib/navigation.dart
@@ -5,6 +5,10 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
 import 'package:klosterguide/constants.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:klosterguide/videoplayer_fullscreen.dart';
+import 'package:flutter/services.dart';
+import 'package:video_player/video_player.dart';
+
 //import 'package:geolocator/geolocator.dart'; Ist für regelmäßige Positionsabfrage nötig. Zurzeit unnötig. NICHT LÖSCHEN! ~ Lukas
 
 import 'data.dart';
@@ -115,43 +119,297 @@ class Navigation extends StatelessWidget {
           ),
         ),
         body: Stack(
+          alignment: Alignment.center,
           children: [
-            FlutterMap(
-              options: MapOptions(
-                  center: LatLng(51.07761902538088, 6.752730047496091),
-                  zoom: 15.7,
-                  minZoom: 14,
-                  maxZoom: 18,
-                  swPanBoundary: LatLng(
-                    51.0727696061025,
-                    6.74033047115352,
+            Positioned(
+              bottom: 10,
+              child: Container(
+                width: 290,
+                height: 190,
+                padding: EdgeInsets.only(left: 16, right: 16),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    boxShadow: [BoxShadow(color: Colors.grey, blurRadius: 10.0, spreadRadius: 0.1)]
+                ),
+                child: Container(
+                  padding: EdgeInsets.only(top: 5,bottom: 5),
+                  child: _StationAssetVideo(
+                    videopath: "assets/guidevideos/yee.mp4",
                   ),
-                  nePanBoundary: LatLng(51.0834444361947, 6.76094581023449),
-                  plugins: [const LocationMarkerPlugin()]),
-              layers: [
-                TileLayerOptions(
-                  tileProvider: const AssetTileProvider(),
-                  maxZoom: 18.0,
-                  urlTemplate: 'assets/map/{z}/{x}/{y}.png',
                 ),
-                MarkerLayerOptions(
-                  markers: [
-                    Marker(
-                      width: 50.0,
-                      height: 50.0,
-                      point: LatLng(stationInfo.xcoord, stationInfo.ycoord),
-                      builder: (ctx) => ClipRRect(
-                        borderRadius: BorderRadius.circular(72.5),
-                        child: Image.asset(stationInfo.iconImage,
-                            width: 200, height: 200),
-                      ),
-                    ),
+
+                /*child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    IconButton(
+                      onPressed: (){
+
+                      },
+
+                        icon: Icon(Icons.play_arrow, color: Colors.grey, size: 30 )),
+                    IconButton(
+                        onPressed: (){
+
+                        },
+
+                        icon: Icon(Icons.play_arrow, color: Colors.grey, size: 30 ))
                   ],
-                ),
-                LocationMarkerLayerOptions(),
-              ],
+                )*/
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(bottom: 200),
+              child: FlutterMap(
+                options: MapOptions(
+                    center: LatLng(51.07761902538088, 6.752730047496091),
+                    zoom: 15.7,
+                    minZoom: 14,
+                    maxZoom: 18,
+                    swPanBoundary: LatLng(
+                      51.0727696061025,
+                      6.74033047115352,
+                    ),
+                    nePanBoundary: LatLng(51.0834444361947, 6.76094581023449),
+                    plugins: [const LocationMarkerPlugin()]),
+                layers: [
+                  TileLayerOptions(
+                    tileProvider: const AssetTileProvider(),
+                    maxZoom: 18.0,
+                    urlTemplate: 'assets/map/{z}/{x}/{y}.png',
+                  ),
+                  MarkerLayerOptions(
+                    markers: [
+                      Marker(
+                        width: 50.0,
+                        height: 50.0,
+                        point: LatLng(stationInfo.xcoord, stationInfo.ycoord),
+                        builder: (ctx) => ClipRRect(
+                          borderRadius: BorderRadius.circular(72.5),
+                          child: Image.asset(stationInfo.iconImage,
+                              width: 200, height: 200),
+                        ),
+                      ),
+                    ],
+                  ),
+                  LocationMarkerLayerOptions(),
+                ],
+              ),
             ),
           ],
         ));
   }
 }
+class _StationAssetVideo extends StatefulWidget {
+  final String videopath;
+
+  const _StationAssetVideo({Key? key, required this.videopath})
+      : super(key: key);
+
+  @override
+  _StationAssetVideoState createState() =>
+      // Falsche interpretation der IDE
+  // ignore: no_logic_in_create_state
+  _StationAssetVideoState(videopath: videopath);
+}
+
+class _StationAssetVideoState extends State<_StationAssetVideo> {
+  late VideoPlayerController _controller;
+  final String videopath;
+
+  _StationAssetVideoState({required this.videopath});
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.asset(videopath);
+
+    _controller.addListener(() {
+      setState(() {});
+    });
+    _controller.setLooping(false);
+    _controller.initialize().then((_) => setState(() {}));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+        overlays:
+        SystemUiOverlay.values); // Benachrichtigungsleiste wieder zeigen
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.bottomCenter,
+      children: <Widget>[
+        VideoPlayerFullscreenWidget(controller: _controller),
+      ],
+    );
+  }
+}
+
+// Video stuff
+class VideoPlayerFullscreenWidget extends StatelessWidget {
+  final VideoPlayerController controller;
+
+  VideoPlayerFullscreenWidget({Key? key, required this.controller})
+      : super(key: key);
+
+  bool isPortrait = true;
+
+  @override
+  Widget build(BuildContext context) => controller.value.isInitialized
+      ? Container(alignment: Alignment.topCenter, child: buildVideo())
+      : const Center(child: CircularProgressIndicator());
+
+  Widget buildVideo() => OrientationBuilder(
+    builder: (context, orientation) {
+      return Stack(
+        fit: isPortrait ? StackFit.loose : StackFit.expand,
+        children: <Widget>[
+          buildVideoPlayer(),
+          Positioned.fill(
+            child: AdvancedOverlayWidget(
+              controller: controller,
+              onClickedFullScreen: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => videoplayer_fullscreen(
+                            controller: controller)));
+              },
+            ),
+          ),
+        ],
+      );
+    },
+  );
+
+  Widget buildVideoPlayer() => buildFullScreen(
+    child: AspectRatio(
+      aspectRatio: controller.value.aspectRatio,
+      child: VideoPlayer(controller),
+    ),
+  );
+
+  Widget buildFullScreen({
+    required Widget child,
+  }) {
+    final size = controller.value.size;
+    final width = size.width;
+    final height = size.height;
+
+    return FittedBox(
+      fit: BoxFit.cover,
+      child: SizedBox(width: width, height: height, child: child),
+    );
+  }
+}
+
+class AdvancedOverlayWidget extends StatelessWidget {
+  final VideoPlayerController controller;
+  final VoidCallback onClickedFullScreen;
+
+  static const allSpeeds = <double>[0.25, 0.5, 1, 1.25, 1.5, 2];
+
+  const AdvancedOverlayWidget(
+      {Key? key, required this.controller, required this.onClickedFullScreen})
+      : super(key: key);
+
+  String getPosition() {
+    final duration = Duration(
+        milliseconds: controller.value.position.inMilliseconds.round());
+
+    return [duration.inMinutes, duration.inSeconds]
+        .map((seg) => seg.remainder(60).toString().padLeft(2, '0'))
+        .join(':');
+  }
+
+  @override
+  Widget build(BuildContext context) => GestureDetector(
+    behavior: HitTestBehavior.opaque,
+    onTap: () =>
+    controller.value.isPlaying ? controller.pause() : controller.play(),
+    child: Stack(
+      children: <Widget>[
+        buildPlay(),
+        buildSpeed(),
+        Align(
+          alignment: Alignment.topLeft,
+          child: Text(getPosition()),
+        ),
+        Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Row(
+              children: [
+                Expanded(child: buildIndicator()),
+                const SizedBox(width: 12),
+                GestureDetector(
+                  child: const Icon(
+                    Icons.fullscreen,
+                    color: Colors.white,
+                    size: 28,
+                  ),
+                  onTap: onClickedFullScreen,
+                ),
+                const SizedBox(width: 8),
+              ],
+            )),
+      ],
+    ),
+  );
+
+  Widget buildIndicator() => Container(
+    margin: const EdgeInsets.all(8).copyWith(right: 0),
+    height: 16,
+    child: VideoProgressIndicator(
+      controller,
+      allowScrubbing: true,
+    ),
+  );
+
+  Widget buildSpeed() => Align(
+    alignment: Alignment.topRight,
+    child: PopupMenuButton<double>(
+      initialValue: controller.value.playbackSpeed,
+      tooltip: 'Playback speed',
+      onSelected: controller.setPlaybackSpeed,
+      itemBuilder: (context) => allSpeeds
+          .map<PopupMenuEntry<double>>((speed) => PopupMenuItem(
+        value: speed,
+        child: Text('${speed}x'),
+      ))
+          .toList(),
+      child: Container(
+        color: Colors.white38,
+        padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+        child: const Text("Tempo"),
+      ),
+    ),
+  );
+
+  Widget buildPlay() => controller.value.isPlaying
+      ? Container()
+      : Container(
+    color: Colors.black26,
+    child: const Center(
+      child: Icon(
+        Icons.play_arrow,
+        color: Colors.white,
+        size: 70,
+      ),
+    ),
+  );
+}
+
+
