2f6ee362f4f67cdab396a07af763200a93db7c8f
LukasGasp <72341833+LukasGasp@users.noreply.github.com>
2023-03-14 16:00:07 +0100

    Dummes "feature" entfernt
    @Denormos Dachte es wäre ein gutes Easter Egg, dass die App mit einer Wahrscheinlichkeit von 1/10 abstürzt.

--- lib/splash.dart
+++ lib/splash.dart
@@ -11,47 +11,43 @@ class SplashScreen extends StatefulWidget {
   Splash createState() => Splash();
 }
 
-class Splash extends State<SplashScreen>  {
-
+class Splash extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-
   }
+
   Random r = Random();
 
   @override
   Widget build(BuildContext context) {
     Timer(
-        Duration(seconds: 2),
-            () =>{
-        if(r.nextInt(10)==5){
-              throw new Exception()
-            },
+        const Duration(seconds: 2),
+        () => {
+              Navigator.of(context).pushReplacement(MaterialPageRoute(
+                  builder: (BuildContext context) =>
+                      const MyHomePage(title: 'Klosterführer Knechtsteden')))
+            });
 
-        Navigator.of(context).pushReplacement(MaterialPageRoute(
-                  builder: (BuildContext context) => MyHomePage(title: 'Klosterführer Knechtsteden')))});
-
-
-
-    var assetsImage = new AssetImage(
+    var assetsImage = const AssetImage(
         "assets/icons/app2.png"); //<- Creates an object that fetches an image.
-    var image = new Image(
+    var image = Image(
         image: assetsImage,
-        height: MediaQuery.of(context).size.width/2); //<- Creates a widget that displays an image.
+        height: MediaQuery.of(context).size.width /
+            2); //<- Creates a widget that displays an image.
 
     return Scaffold(
-        /* appBar: AppBar(
+      /* appBar: AppBar(
           title: Text("MyApp"),
           backgroundColor:
               Colors.blue, //<- background color to combine with the picture :-)
         ),*/
-        body: Container(
-          decoration: new BoxDecoration(color: Colors.white),
-          child: new Center(
-            child: image,
-          ),
-        ), //<- place where the image appears
-      );
+      body: Container(
+        decoration: const BoxDecoration(color: Colors.white),
+        child: Center(
+          child: image,
+        ),
+      ), //<- place where the image appears
+    );
   }
 }
