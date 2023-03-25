47c2d663dc821171a30ff1442e8400c9ce67e5e9
LukasGasp <72341833+LukasGasp@users.noreply.github.com>
2023-03-25 23:14:03 +0100

    _StationAssetVideo Modulariesiert
    https://studyflix.de/informatik/modularisierung-825
    
    Spart viel Speicher und ist super

--- lib/navigation_detail_page.dart
+++ lib/navigation_detail_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:klosterguide/navigation_endcard.dart';
-import 'package:klosterguide/videoplayer_fullscreen.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'main.dart';
@@ -8,9 +7,10 @@ import 'navigation.dart';
 import 'data.dart';
 import 'dart:io'; // Um files einzulesen
 import 'constants.dart';
-import 'package:video_player/video_player.dart';
 
-import 'package:flutter/services.dart'; // Um Rotation festzulegen. Flutter Native...
+// Um Rotation festzulegen. Flutter Native...
+
+import 'StationAssetVideo.dart';
 
 class DetailPage extends StatelessWidget {
   final List tourlist;
@@ -237,7 +237,7 @@ class DetailPage extends StatelessWidget {
                       }
                       final file = snapshot.data;
                       if (file != null) {
-                        return _StationAssetVideo(videopath: file);
+                        return StationAssetVideo(videopath: file);
                       } else {
                         return const Text('File not found');
                       }
@@ -318,7 +318,7 @@ class DetailPage extends StatelessWidget {
                                 }
                                 final file = snapshot.data;
                                 if (file != null) {
-                                  return _StationAssetVideo(videopath: file);
+                                  return StationAssetVideo(videopath: file);
                                 } else {
                                   return const Text('File not found');
                                 }
@@ -407,218 +407,4 @@ class DetailPage extends StatelessWidget {
       ),
     );
   }
-}
-
-// Video stuff
-class _StationAssetVideo extends StatefulWidget {
-  final File videopath;
-
-  const _StationAssetVideo({Key? key, required this.videopath})
-      : super(key: key);
-
-  @override
-  _StationAssetVideoState createState() =>
-      // Falsche interpretation der IDE
-      // ignore: no_logic_in_create_state
-      _StationAssetVideoState(videopath: videopath);
-}
-
-class _StationAssetVideoState extends State<_StationAssetVideo> {
-  late VideoPlayerController _controller;
-  final File videopath;
-
-  _StationAssetVideoState({required this.videopath});
-
-  var dir;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = VideoPlayerController.file(videopath);
-    _controller.addListener(() {
-      setState(() {});
-    });
-    _controller.setLooping(false);
-    _controller.initialize().then((_) => setState(() {}));
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
-      DeviceOrientation.portraitDown,
-    ]);
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
-        overlays:
-            SystemUiOverlay.values); // Benachrichtigungsleiste wieder zeigen
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.bottomCenter,
-      children: <Widget>[
-        VideoPlayerFullscreenWidget(controller: _controller),
-      ],
-    );
-  }
-}
-
-// Video stuff
-class VideoPlayerFullscreenWidget extends StatelessWidget {
-  final VideoPlayerController controller;
-
-  VideoPlayerFullscreenWidget({Key? key, required this.controller})
-      : super(key: key);
-
-  bool isPortrait = true;
-
-  @override
-  Widget build(BuildContext context) => controller.value.isInitialized
-      ? Container(alignment: Alignment.topCenter, child: buildVideo())
-      : const Center(child: CircularProgressIndicator.adaptive());
-
-  Widget buildVideo() => OrientationBuilder(
-        builder: (context, orientation) {
-          return Stack(
-            fit: isPortrait ? StackFit.loose : StackFit.expand,
-            children: <Widget>[
-              buildVideoPlayer(),
-              Positioned.fill(
-                child: AdvancedOverlayWidget(
-                  controller: controller,
-                  onClickedFullScreen: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => videoplayer_fullscreen(
-                                controller: controller)));
-                  },
-                ),
-              ),
-            ],
-          );
-        },
-      );
-
-  Widget buildVideoPlayer() => buildFullScreen(
-        child: AspectRatio(
-          aspectRatio: controller.value.aspectRatio,
-          child: VideoPlayer(controller),
-        ),
-      );
-
-  Widget buildFullScreen({
-    required Widget child,
-  }) {
-    final size = controller.value.size;
-    final width = size.width;
-    final height = size.height;
-
-    return FittedBox(
-      fit: BoxFit.cover,
-      child: SizedBox(width: width, height: height, child: child),
-    );
-  }
-}
-
-class AdvancedOverlayWidget extends StatelessWidget {
-  final VideoPlayerController controller;
-  final VoidCallback onClickedFullScreen;
-
-  static const allSpeeds = <double>[0.25, 0.5, 1, 1.25, 1.5, 2];
-
-  const AdvancedOverlayWidget(
-      {Key? key, required this.controller, required this.onClickedFullScreen})
-      : super(key: key);
-
-  String getPosition() {
-    final duration = Duration(
-        milliseconds: controller.value.position.inMilliseconds.round());
-
-    return [duration.inMinutes, duration.inSeconds]
-        .map((seg) => seg.remainder(60).toString().padLeft(2, '0'))
-        .join(':');
-  }
-
-  @override
-  Widget build(BuildContext context) => GestureDetector(
-        behavior: HitTestBehavior.opaque,
-        onTap: () =>
-            controller.value.isPlaying ? controller.pause() : controller.play(),
-        child: Stack(
-          children: <Widget>[
-            buildPlay(),
-            buildSpeed(),
-            Align(
-              alignment: Alignment.topLeft,
-              child: Text(getPosition()),
-            ),
-            Positioned(
-                bottom: 0,
-                left: 0,
-                right: 0,
-                child: Row(
-                  children: [
-                    Expanded(child: buildIndicator()),
-                    const SizedBox(width: 12),
-                    GestureDetector(
-                      child: const Icon(
-                        Icons.fullscreen,
-                        color: Colors.white,
-                        size: 28,
-                      ),
-                      onTap: onClickedFullScreen,
-                    ),
-                    const SizedBox(width: 8),
-                  ],
-                )),
-          ],
-        ),
-      );
-
-  Widget buildIndicator() => Container(
-        margin: const EdgeInsets.all(8).copyWith(right: 0),
-        height: 16,
-        child: VideoProgressIndicator(
-          controller,
-          allowScrubbing: true,
-        ),
-      );
-
-  Widget buildSpeed() => Align(
-        alignment: Alignment.topRight,
-        child: PopupMenuButton<double>(
-          initialValue: controller.value.playbackSpeed,
-          tooltip: 'Playback speed',
-          onSelected: controller.setPlaybackSpeed,
-          itemBuilder: (context) => allSpeeds
-              .map<PopupMenuEntry<double>>((speed) => PopupMenuItem(
-                    value: speed,
-                    child: Text('${speed}x'),
-                  ))
-              .toList(),
-          child: Container(
-            color: Colors.white38,
-            padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-            child: const Text("Tempo"),
-          ),
-        ),
-      );
-
-  Widget buildPlay() => controller.value.isPlaying
-      ? Container()
-      : Container(
-          color: Colors.black26,
-          child: const Center(
-            child: Icon(
-              Icons.play_arrow,
-              color: Colors.white,
-              size: 70,
-            ),
-          ),
-        );
 }
--- lib/spenden.dart
+++ lib/spenden.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'main.dart';
 import 'constants.dart';
@@ -7,6 +9,7 @@ import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter/services.dart'; // Um Rotation festzulegen. Flutter Native...
 
 import 'videoplayer_fullscreen.dart';
+import 'StationAssetVideo.dart';
 
 class Spenden extends StatelessWidget {
   const Spenden({Key? key}) : super(key: key);
@@ -64,81 +67,82 @@ class Spenden extends StatelessWidget {
                     //Spenden
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: <Widget>[
-                      Column(
-                        children: [
-                          SizedBox(
-                            height: 50,
-                            width: (MediaQuery.of(context).size.width>=500)?180:MediaQuery.of(context).size.width*180*0.002,
-                            child: FloatingActionButton(
-                              backgroundColor: primarybuttoncolor,
-                              shape: const RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.only(
-                                bottomLeft: Radius.circular(10),
-                                bottomRight: Radius.circular(10),
-                                topLeft: Radius.circular(10),
-                                topRight: Radius.circular(10),
-                              )),
-                              child: Container(
-                                  alignment: Alignment.center,
-                                  child: Text(
-                                    DemoLocalizations.of(context)!
-                                        .getText("endcard2ndbutton"),
-                                    textAlign: TextAlign.center,
-                                  )),
-                              onPressed: () {
-                                launch(DemoLocalizations.of(context)!
-                                    .getText("spendenlinkmh"));
-                              },
-                            ),
+                      Column(children: [
+                        SizedBox(
+                          height: 50,
+                          width: (MediaQuery.of(context).size.width >= 500)
+                              ? 180
+                              : MediaQuery.of(context).size.width * 180 * 0.002,
+                          child: FloatingActionButton(
+                            backgroundColor: primarybuttoncolor,
+                            shape: const RoundedRectangleBorder(
+                                borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(10),
+                              bottomRight: Radius.circular(10),
+                              topLeft: Radius.circular(10),
+                              topRight: Radius.circular(10),
+                            )),
+                            child: Container(
+                                alignment: Alignment.center,
+                                child: Text(
+                                  DemoLocalizations.of(context)!
+                                      .getText("endcard2ndbutton"),
+                                  textAlign: TextAlign.center,
+                                )),
+                            onPressed: () {
+                              launch(DemoLocalizations.of(context)!
+                                  .getText("spendenlinkmh"));
+                            },
                           ),
-                          SizedBox(
-                            height: 20,
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        SizedBox(
+                          child:
+                              Image.asset('assets/icons/logo-missionshaus.png'),
+                          height: 100,
+                          width: 100,
+                        ),
+                      ]),
+                      Column(children: [
+                        SizedBox(
+                          height: 50,
+                          width: (MediaQuery.of(context).size.width >= 500)
+                              ? 180
+                              : MediaQuery.of(context).size.width * 180 * 0.002,
+                          child: FloatingActionButton(
+                            backgroundColor: primarybuttoncolor,
+                            shape: const RoundedRectangleBorder(
+                                borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(10),
+                              bottomRight: Radius.circular(10),
+                              topLeft: Radius.circular(10),
+                              topRight: Radius.circular(10),
+                            )),
+                            child: Container(
+                                alignment: Alignment.center,
+                                child: Text(
+                                  DemoLocalizations.of(context)!
+                                      .getText("endcard3rdbutton"),
+                                  textAlign: TextAlign.center,
+                                )),
+                            onPressed: () {
+                              launch(DemoLocalizations.of(context)!
+                                  .getText("spendenlinkngk"));
+                            },
                           ),
-                          SizedBox(
-                            child:
-                            Image.asset('assets/icons/logo-missionshaus.png'),
-                            height: 100,
-                            width: 100,
-                          ),
-                        ]
-                      ),
-                      Column(
-                        children: [
-                          SizedBox(
-                            height: 50,
-                            width: (MediaQuery.of(context).size.width>=500)?180:MediaQuery.of(context).size.width*180*0.002,
-                            child: FloatingActionButton(
-                              backgroundColor: primarybuttoncolor,
-                              shape: const RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.only(
-                                bottomLeft: Radius.circular(10),
-                                bottomRight: Radius.circular(10),
-                                topLeft: Radius.circular(10),
-                                topRight: Radius.circular(10),
-                              )),
-                              child: Container(
-                                  alignment: Alignment.center,
-                                  child: Text(
-                                    DemoLocalizations.of(context)!
-                                        .getText("endcard3rdbutton"),
-                                    textAlign: TextAlign.center,
-                                  )),
-                              onPressed: () {
-                                launch(DemoLocalizations.of(context)!
-                                    .getText("spendenlinkngk"));
-                              },
-                            ),
-                          ),
-                          SizedBox(
-                            height: 20,
-                          ),
-                          SizedBox(
-                            child: Image.asset('assets/icons/logo-ngk-campus.png'),
-                            height: 100,
-                            width: 100,
-                          ),
-                        ]
-                      ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        SizedBox(
+                          child:
+                              Image.asset('assets/icons/logo-ngk-campus.png'),
+                          height: 100,
+                          width: 100,
+                        ),
+                      ]),
                     ],
                   ),
                   const SizedBox(height: 10),
@@ -160,207 +164,8 @@ class _StationAssetVideo extends StatefulWidget {
       : super(key: key);
 
   @override
-  _StationAssetVideoState createState() =>
+  StationAssetVideoState createState() =>
       // Falsche interpretation der IDE
       // ignore: no_logic_in_create_state
-      _StationAssetVideoState(videopath: videopath);
-}
-
-class _StationAssetVideoState extends State<_StationAssetVideo> {
-  late VideoPlayerController _controller;
-  final String videopath;
-
-  _StationAssetVideoState({required this.videopath});
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = VideoPlayerController.asset(videopath);
-
-    _controller.addListener(() {
-      setState(() {});
-    });
-    _controller.setLooping(false);
-    _controller.initialize().then((_) => setState(() {}));
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
-      DeviceOrientation.portraitDown,
-    ]);
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
-        overlays:
-            SystemUiOverlay.values); // Benachrichtigungsleiste wieder zeigen
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.bottomCenter,
-      children: <Widget>[
-        VideoPlayerFullscreenWidget(controller: _controller),
-      ],
-    );
-  }
-}
-
-// Video stuff
-class VideoPlayerFullscreenWidget extends StatelessWidget {
-  final VideoPlayerController controller;
-
-  VideoPlayerFullscreenWidget({Key? key, required this.controller})
-      : super(key: key);
-
-  bool isPortrait = true;
-
-  @override
-  Widget build(BuildContext context) => controller.value.isInitialized
-      ? Container(alignment: Alignment.topCenter, child: buildVideo())
-      : const Center(child: CircularProgressIndicator());
-
-  Widget buildVideo() => OrientationBuilder(
-        builder: (context, orientation) {
-          return Stack(
-            fit: isPortrait ? StackFit.loose : StackFit.expand,
-            children: <Widget>[
-              buildVideoPlayer(),
-              Positioned.fill(
-                child: AdvancedOverlayWidget(
-                  controller: controller,
-                  onClickedFullScreen: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => videoplayer_fullscreen(
-                                controller: controller)));
-                  },
-                ),
-              ),
-            ],
-          );
-        },
-      );
-
-  Widget buildVideoPlayer() => buildFullScreen(
-        child: AspectRatio(
-          aspectRatio: controller.value.aspectRatio,
-          child: VideoPlayer(controller),
-        ),
-      );
-
-  Widget buildFullScreen({
-    required Widget child,
-  }) {
-    final size = controller.value.size;
-    final width = size.width;
-    final height = size.height;
-
-    return FittedBox(
-      fit: BoxFit.cover,
-      child: SizedBox(width: width, height: height, child: child),
-    );
-  }
-}
-
-class AdvancedOverlayWidget extends StatelessWidget {
-  final VideoPlayerController controller;
-  final VoidCallback onClickedFullScreen;
-
-  static const allSpeeds = <double>[0.25, 0.5, 1, 1.5, 2, 3, 5, 10];
-
-  const AdvancedOverlayWidget(
-      {Key? key, required this.controller, required this.onClickedFullScreen})
-      : super(key: key);
-
-  String getPosition() {
-    final duration = Duration(
-        milliseconds: controller.value.position.inMilliseconds.round());
-
-    return [duration.inMinutes, duration.inSeconds]
-        .map((seg) => seg.remainder(60).toString().padLeft(2, '0'))
-        .join(':');
-  }
-
-  @override
-  Widget build(BuildContext context) => GestureDetector(
-        behavior: HitTestBehavior.opaque,
-        onTap: () =>
-            controller.value.isPlaying ? controller.pause() : controller.play(),
-        child: Stack(
-          children: <Widget>[
-            buildPlay(),
-            buildSpeed(),
-            Align(
-              alignment: Alignment.topLeft,
-              child: Text(getPosition()),
-            ),
-            Positioned(
-                bottom: 0,
-                left: 0,
-                right: 0,
-                child: Row(
-                  children: [
-                    Expanded(child: buildIndicator()),
-                    const SizedBox(width: 12),
-                    GestureDetector(
-                      child: const Icon(
-                        Icons.fullscreen,
-                        color: Colors.white,
-                        size: 28,
-                      ),
-                      onTap: onClickedFullScreen,
-                    ),
-                    const SizedBox(width: 8),
-                  ],
-                )),
-          ],
-        ),
-      );
-
-  Widget buildIndicator() => Container(
-        margin: const EdgeInsets.all(8).copyWith(right: 0),
-        height: 16,
-        child: VideoProgressIndicator(
-          controller,
-          allowScrubbing: true,
-        ),
-      );
-
-  Widget buildSpeed() => Align(
-        alignment: Alignment.topRight,
-        child: PopupMenuButton<double>(
-          initialValue: controller.value.playbackSpeed,
-          tooltip: 'Playback speed',
-          onSelected: controller.setPlaybackSpeed,
-          itemBuilder: (context) => allSpeeds
-              .map<PopupMenuEntry<double>>((speed) => PopupMenuItem(
-                    value: speed,
-                    child: Text('${speed}x'),
-                  ))
-              .toList(),
-          child: Container(
-            color: Colors.white38,
-            padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-            child: Text('${controller.value.playbackSpeed}x'),
-          ),
-        ),
-      );
-
-  Widget buildPlay() => controller.value.isPlaying
-      ? Container()
-      : Container(
-          color: Colors.black26,
-          child: const Center(
-            child: Icon(
-              Icons.play_arrow,
-              color: Colors.white,
-              size: 70,
-            ),
-          ),
-        );
+      StationAssetVideoState(videopath: File(videopath));
 }
